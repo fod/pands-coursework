43e48b8147f276a6ab9c455e574e8a2e93a74849
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-02-09 09:57:46 +0000

    improved comments in isEven.py

--- week04-flow/isEven.py
+++ week04-flow/isEven.py
@@ -6,7 +6,14 @@
 # apply to numbers with a non-zero fractional part
 num = int(input("Please enter an integer: "))
 
+# If the number is evenly divisible by 2 then it is even
+# The modulus operator returns the remainder of a division operation 
+# ... therefore if num % 2 is equal to 0 then num is even
 if (num % 2 == 0):
+
+    # If the number is even print this message
     print("The number {} is even".format(num))
 else:
+    
+    # otherwise print this message
     print("Then number {} is odd".format(num))
