adf58fa4d6d417531d030b91839889afc24ea216
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-02-10 03:02:17 +0000

    evens.py --> print even numbers

---
+++ week04-loops/evens.py
@@ -0,0 +1,12 @@
+# evens.py
+# Use a while loop to print all even numbers from 2 to to 100
+# Author: Fiachra O' Donoghue
+
+# Initialise the counter variable
+counter = 2
+
+# This loop will repeatedly print the counter variable then increment it by 2 until the 
+# stopping condition (counter < 101) is met
+while (counter < 101):
+    print (counter)
+    counter += 2
