e844f83bd67d4b556595d248ffd1d62c25b48830
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-02-16 16:33:11 +0000

    Changed structure of student in student_dict.py

--- week05-data_structures/student_dict.py
+++ week05-data_structures/student_dict.py
@@ -3,8 +3,20 @@
 # Author: Fiachra O' Donoghue
 
 # Create a dictionary containing student information
-student = {"Name": "Mary", "Programming": 45, "History": 99}
+# Personal details are in a sub-dictionary so that they can be accessed like:
+# student["PerosnalDetails"]["Name"], etc
+# and grades are in a list of dictionaries so that they can be iterated through easily
+student = {"PersonalDetails": 
+                {"Name": "Mary"},
+            "Grades": [
+                {"Course": "Programming", "Grade": 45},
+                {"Course": "History", "Grade": 99 }
+            ]
+           }
 
+# Print STudent's personal details
+print("Student: {}".format(student["PersonalDetails"]["Name"]))
 
-for k, v in student.items():
-    print("{}: {}".format(k, v))
+# Iterate through the "Grades" list and extract the course name and grade from each (dict) item
+for course in student["Grades"]:
+    print("\t{}: {}".format(course["Course"], course["Grade"]))
