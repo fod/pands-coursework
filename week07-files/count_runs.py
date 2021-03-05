1771eeeddf91f45da8c5aa270c0ded513089f10d
Fiachra O' Donoghue <fiachra.m.odonoghue@gmail.com>
2021-03-05 09:58:27 +0000

    refactored count_runs.py to use read and write functions

--- week07-files/count_runs.py
+++ week07-files/count_runs.py
@@ -2,20 +2,40 @@
 # A program that records the number of times it has been run
 # Author: Fiachra O' Donoghue
 
+# Count-reading function
+def readCount(filename):
+
+    # Read current value from countFile
+    with open(filename, "r") as file:
+        count = int(file.read())
+
+        # return count to caller
+        return count
+
+
+# Count-writing function
+def writeCount(filename, count):
+
+    # Open the file for writing
+    with open(filename, "w") as file:
+
+        # Write the new run count to the file
+        file.write(str(count))
+
+
 # Location of the file that stores the count of programme runs
 countFile = "count.txt"
-count = 0
 
-# Read current value from countFile
-with open(countFile) as file:
-    count = int(file.read())
+# Read the current dount from the count file
+count = readCount(countFile)
 
 # Increment the count
 count += 1
 
-# Open the file for writing
-with open(countFile, "w") as file:
+# Write out the new count
+writeCount(countFile, count)
 
-    # Write the new run count to the file
-    file.write(str(count))
+
+
+
 
